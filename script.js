cdf51cece5285f64019cebec5859036fcdcc3b82
tadeulps <tadeslopes@gmail.com>
2021-04-15 02:58:53 -0300

    Mensagens carregadas e exibidas conforme o layout

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 
-const promessa=axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages')
+const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages')
 promessa.then(popularMensagens);
 
 function popularMensagens(resposta) {
 	console.log(resposta.data);
-	mensagens=resposta.data;
+	mensagens = resposta.data;
 	colocarMensagem()
 	console.log(mensagens.length)
 }
 
-function colocarMensagem(){
-	const caixaDeMensagens=document.querySelector(".caixa-de-mensagens");
-	caixaDeMensagens.innerHTML+="";
-  for(let i=0;i<mensagens.length;i++){
-		caixaDeMensagens.innerHTML+=`
-		<div class="mensagem status"><strong>${mensagens[i].from}</strong> entra na sala</div>
-		
-		
-		
-		
-		`
+function colocarMensagem() {
+	const caixaDeMensagens = document.querySelector(".caixa-de-mensagens");
+	caixaDeMensagens.innerHTML += "";
+	for (let i = 0; i < mensagens.length; i++) {
+		if (mensagens[i].type == 'status') {
+			caixaDeMensagens.innerHTML += `
+		<div class="mensagem status"><strong>${mensagens[i].from}</strong> ${mensagens[i].text}</div>`
+		}
+		else if (mensagens[i].type == 'message') {
+			caixaDeMensagens.innerHTML += `
+		 <div class="mensagem normal"><strong>${mensagens[i].from}</strong> para <strong>${mensagens[i].to}</strong> : ${mensagens[i].text}</div>`
+		}
+		else if (mensagens[i].type == 'private_message') {
+			caixaDeMensagens.innerHTML += `
+		<div class="mensagem reservada"><strong>${mensagens[i].from}</strong> reservadamente para <strong>${mensagens[i].to}</strong>: ${mensagens[i].text}</div>`
+		}
+	}
 }
-}
+private_message
